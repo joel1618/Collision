a4b1edefad5f59a679179c637ee83d39575bd27a
Joel Schiffer <joel1618@gmail.com>
2016-04-11 21:13:56 -0500

    Added user controller

---
+++ Collision.Core/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collision.Core.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+    }
+}
---
+++ Collision/Controllers/v1/UserApiController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Net;
+using System.Net.Http;
+using Collision.Sql.Ef.Repositories.Interfaces;
+using Collision.ViewModels.Extensions;
+using Microsoft.AspNet.Identity;
+using UserCore = Collision.Core.Models.User;
+
+namespace Collision.Controllers.v1
+{
+    public class UserApiController : ApiController
+    {
+        private readonly IUserSettingsRepository _userRepository;
+        public UserApiController(IUserSettingsRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public UserCore GetCurrent()
+        {
+            return new UserCore()
+            {
+                Id = User.Identity.GetUserId(),
+                Username = User.Identity.GetUserName()
+            };
+        }
+    }
+}
