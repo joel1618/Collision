60f39b1d8706c0548c5f2fcaf72d0e7fd1c613fc
Joel Schiffer <joel1618@gmail.com>
2016-04-20 21:21:55 -0500

    Revert "fix bug to positionservice.js.  modify controlpanelcontroller to use positionservice.js"
    This reverts commit e9e57002d1b5877a4c969df0cdab14da0fa03ca9.

--- Collision.Web/App/controlpanel/controllers/controlpanelcontroller.js
+++ Collision.Web/App/controlpanel/controllers/controlpanelcontroller.js
@@ -4,23 +4,15 @@
         id: null, entity: null
     };
     var entities = [];
-    angular.module('controlpanel').controller('controlpanelcontroller', ['$scope', '$http', '$timeout', 'breezeservice', 'breeze', 'aircraftservice', 'positionservice',
-    function controller($scope, $http, $timeout, breezeservice, breeze, aircraftservice, positionservice) {
+    angular.module('controlpanel').controller('controlpanelcontroller', ['$scope', '$http', '$timeout', 'breezeservice', 'breeze', 'aircraftservice',
+    function controller($scope, $http, $timeout, breezeservice, breeze, aircraftservice) {
         $scope.isLoading = true;
         var entities = [];
         navigator.geolocation.getCurrentPosition(function (position) {
             $scope.position = position;
-            //aircraftservice.GetPositions($scope.position).then(function (data) {
-            //    $scope.positions = data;
-            //});
-            var p1 = new breeze.Predicate('Latitude2', '>', position.coords.latitude - .5);
-            var p2 = new breeze.Predicate('Latitude2', '<', position.coords.latitude + .5);
-            var p3 = new breeze.Predicate('Longitude2', '>', position.coords.longitude - .5);
-            var p4 = new breeze.Predicate('Longitude2', '<', position.coords.longitude + .5);
-            var predicate = new breeze.Predicate.and([p1, p2, p3, p4]);
-            positionservice.search(null, 0, 10, null).then(function (data) {
+            aircraftservice.GetPositions($scope.position).then(function (data) {
                 $scope.positions = data;
-            })
+            });
             aircraftservice.GetConflicts().then(function (data) {
                 $scope.conflicts = data;
             });
