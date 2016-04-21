494a766d4375cab7e28573b3f4c1156213b4a1bd
Joel Schiffer <joel1618@gmail.com>
2016-04-20 21:17:18 -0500

    Add database script

---
+++ Collision.Database/dbo/Tables/UserSettings.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[UserSettings] (
+    [Id]       NVARCHAR (128) NOT NULL,
+    [UserId]   NVARCHAR (128) NOT NULL,
+    [Distance] NVARCHAR (10)  NOT NULL,
+    CONSTRAINT [PK_UserSettings] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_UserSettings_AspNetUsers] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id])
+);
+
